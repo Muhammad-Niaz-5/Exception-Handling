560a90ce7fed2942d4f9dbecb5794c167c80872a
Muhammad-Niaz-5 <Zainsarfaraz1996@gmail.com>
2018-04-03 18:20:55 +0500

    first commit

---
+++ OOP Lab 9 ques 1/OOP Lab 9 ques 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP_Lab_9_ques_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            float ans;
+        start: try
+            {
+                Console.WriteLine("Enter a number: ");
+                number = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter a number to be divided: " + number);
+                int numb = int.Parse(Console.ReadLine());
+                ans = numb / number;
+            }
+
+            catch
+            {
+                Console.WriteLine("Invalid!! \n .....Enter number other than zero \n ... Press Enter To Repeat");
+
+                Console.ReadKey();
+                goto start;
+            }
+
+            Console.WriteLine("ans is: " + ans);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP lab 9 ques 2/OOP lab 9 ques 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP_lab_9_ques_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long id;
+            string name;
+        start: try
+            {
+                Console.WriteLine("Library System...");
+                Console.WriteLine("Enter your Id Card No.: ");
+                id = long.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Book Name: ");
+                name = Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("\nInvalid!!! \nId Card No. should be in number! \n Press Enter to Continue");
+                Console.ReadKey();
+                goto start;
+            }
+            if (name == "Turbo C")
+            {
+                Console.WriteLine("Sorry this book has already been issued to someone! \n... Press enter to repeat");
+                Console.ReadKey();
+                goto start;
+            }
+            else
+            {
+                Console.WriteLine(name + " is now issued to ID No.:" + id);
+            }
+            Console.ReadKey();
+        }
+    }
+}
